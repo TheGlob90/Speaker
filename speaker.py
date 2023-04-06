b8003c4090177a4450a76ffe8a2564bd21a24499
Brandon Lefert <brandon.lefert@gmail.com>
2023-04-06 11:45:25 -0500

    Change pin

--- speaker.py
+++ speaker.py
@@ -2,7 +2,7 @@ import pwmio
 import board
 import time
 
-pwm = pwmio.PWMOut(board.D32, duty_cycle=2 ** 15, frequency=440, variable_frequency=True)
+pwm = pwmio.PWMOut(board.D12, duty_cycle=2 ** 15, frequency=440, variable_frequency=True)
 time.sleep(0.2)
 pwm.frequency = 880
 time.sleep(0.1)
