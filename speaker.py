bbf0a282baa8c94a91948c064076bc1b5150bbd7
Brandon Lefert <brandon.lefert@gmail.com>
2023-04-06 11:48:56 -0500

    Loop

--- speaker.py
+++ speaker.py
@@ -3,6 +3,7 @@ import board
 import time
 
 pwm = pwmio.PWMOut(board.D12, duty_cycle=2 ** 15, frequency=440)
-time.sleep(0.2)
-pwm.frequency = 880
-time.sleep(0.1)
+while True:
+    time.sleep(0.2)
+    pwm.frequency = 880
+    time.sleep(0.1)
